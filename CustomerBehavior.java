1bfe8001fbec364a60db887053f6b98f4362ef57
user <aast7950@colorado.edu>
2019-10-09 14:43:10 -0600

    made base customer files

---
+++ CustomerBehavior.java
@@ -0,0 +1,26 @@
+//
+//  CustomerBehavior.h
+//  OOAD Project 3
+//
+
+public class CustomerBehavior{
+
+
+	
+}
+
+// #ifndef NoiseBehavior_hpp
+// #define NoiseBehavior_hpp
+
+// #include <stdio.h>
+// #include <iostream>
+// #include <string>
+
+// Strategy pattern applied here
+// class NoiseBehavior {
+// public:
+//     //pure virtual function
+//     virtual std::string makeNoise() = 0;
+// };
+
+// #endif /* NoiseBehavior_hpp */
