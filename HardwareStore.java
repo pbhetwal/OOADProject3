d2a079965553e6ab2b717aca3ea8492f191fb6b5
pbhetwal <pari.bhetwal@gmail.com>
2019-10-09 13:36:17 -0600

    update

--- HardwareStore.java
+++ HardwareStore.java
@@ -1,6 +1,9 @@
+// Singleton pattern used here 
 class HardwareStore{
 	private static HardwareStore ins = new HardwareStore();
-	private HardwareStore(){}
+	private HardwareStore(){
+		System.out.println("Our hardware rental store is now open!")
+	}
 	public static HardwareStore getInstance(){
 		return ins; 
 	}
