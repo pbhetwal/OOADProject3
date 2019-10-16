c43ceaddf25fddcbbf0dadbf9d96caa2d6fef94a
pbhetwal <pari.bhetwal@gmail.com>
2019-10-16 16:25:19 -0600

    update

--- Observer.java
+++ Observer.java
@@ -1,4 +1,8 @@
 public interface Observer {
+	/*
+	:Description:	Observer pattern used here - this is the interface 
+					for the observer.
+	*/
     public void update();
     public void announce();
 }
