caa49f3047878ca820d8d39d15ae23cd7a385ae6
pbhetwal <pari.bhetwal@gmail.com>
2019-10-16 16:34:01 -0600

    comments

--- DisplayElement.java
+++ DisplayElement.java
@@ -1,3 +1,7 @@
 public interface DisplayElement {
+	/*
+	:Description:	Observer pattern used here - this is the interface 
+					for the observer to announce an event. 
+	*/
     public void announce();
 }
