2bf92dba425610fea265da4de607310be89b3dc2
pbhetwal <pari.bhetwal@gmail.com>
2019-10-16 16:22:37 -0600

    update

--- Subject.java
+++ Subject.java
@@ -1,4 +1,8 @@
 public interface Subject<T> {
+	/*
+	:Description:	Observer pattern used here - this is the interface 
+					for the observable. 
+	*/
     public void registerObserver(T observer);
     public void removeObserver();
     public void notifyObserver();
